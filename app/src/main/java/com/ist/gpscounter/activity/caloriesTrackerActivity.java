bb022dea80251cac2b1872c7989562b2ea1d6e3b
Olusegun14 <96031584+Olusegun14@users.noreply.github.com>
2022-07-14 18:04:46 +0100

    Creation of calories activity

--- app/src/main/java/com/ist/gpscounter/activity/caloriesTrackerActivity.java
+++ app/src/main/java/com/ist/gpscounter/activity/caloriesTrackerActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.ist.gpscounter.R;
+
 public class caloriesTrackerActivity extends AppCompatActivity {
 
     @Override
