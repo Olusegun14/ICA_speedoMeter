6f53a13060f544c0b3af9bf17389c7766e887a40
Olusegun14 <96031584+Olusegun14@users.noreply.github.com>
2022-07-14 19:32:44 +0100

    Changed the layout

--- app/src/main/java/com/ist/gpscounter/activity/caloriesTrackerActivity.java
+++ app/src/main/java/com/ist/gpscounter/activity/caloriesTrackerActivity.java
@@ -6,11 +6,15 @@ import android.os.Bundle;
 
 import com.ist.gpscounter.R;
 
+import org.w3c.dom.Text;
+
 public class caloriesTrackerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calories_tracker);
+
+        Text text;
     }
 }
